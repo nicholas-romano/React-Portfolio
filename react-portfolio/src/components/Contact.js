52ca04a8d3663118f8ac45174240f942385316d2
Nick Romano <njr7romano@yahoo.com>
2020-07-14 17:25:41 -0400

    Added react-hook-form dependency for contact form validation

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useForm } from 'react-hook-form';
 
 const Contact = () => {
 
-  const handleFormSubmit = e => {
-    e.preventDefault();
-    console.log('form submitted');
-  };
+  const { register, handleSubmit, errors } = useForm();
+
+  const [formValues, setFormVal] = useState({
+    name: '',
+    company: '',
+    email: '',
+    phone1: 0,
+    phone2: 0,
+    phone3: 0,
+    message: ''
+  });
+
+  useEffect(() => {
+    //console.log('name: ', formValues.name)
+    //console.log('company: ', formValues.company)
+  }, [formValues]);
+
+  const onSubmit = data => {
+    console.log(data);
+  }
 
   return (
     <main>
@@ -18,51 +35,50 @@ const Contact = () => {
                         what I can do, please don’t hesitate to ask here. I am more than happy
                         to answer questions you have.</p>
                     <p className="required"> * Indicates a required field.</p>
-                    <form id="contact_form" className="form-horizontal" onSubmit={handleFormSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)} id="contact_form" name="contact" method="POST" data-netlify="true" className="form-horizontal">
                       <div className="form-group">
                         <label htmlFor="Name" className="col-sm-1 control-label"><span className="required">*</span>Name</label>
                         <div className="col-sm-12">
-                            <input type="text" className="form-control" id="name" name="name" placeholder="Name" maxLength="60" />
-                          <div className="name_error">Name is a required field.</div>
-                          <div className="name_valid_error">Name field is invalid.</div>
+                            <input type="text" onChange={e => setFormVal({ ...formValues, name: e.target.value})} className="form-control" ref={register({required: true, pattern: /^['-\w]+['-\w\s]+$/})} id="name" name="name" placeholder="Name" maxLength="60" />
+                            {errors.name && <p className="error">Name field is invalid</p>}
                         </div>
                       </div>
                       <div className="form-group">
                         <label htmlFor="Company" className="col-sm-1 control-label">Company</label>
                         <div className="col-sm-12">
-                          <input type="text" className="form-control" id="company" name="company" placeholder="Company name" maxLength="60" />
-                          <div className="company_valid_error">Company field is invalid.</div>
+                          <input type="text" onChange={e => setFormVal({ ...formValues, company: e.target.value})} className="form-control" ref={register({pattern: /^['-\w]+['-\w\s]+$/})} id="company" name="company" placeholder="Company name" maxLength="60" />
+                          {errors.company && <p className="error">Company field is invalid</p>}
                         </div>
                       </div>
                       <div className="form-group">
                         <label htmlFor="Email" className="col-sm-1 control-label"><span className="required">*</span>Email</label>
                         <div className="col-sm-12">
-                          <input type="email" className="form-control" id="email" name="email" placeholder="Email" maxLength="60" />
-                          <div className="email_error">Email is a required field.</div>
-                          <div className="email_valid_error">Email field is invalid.</div>
+                          <input type="email" onChange={e => setFormVal({ ...formValues, email: e.target.value})} className="form-control" ref={register({required: true, pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})$/})} id="email" name="email" placeholder="Email" maxLength="60" />
+                          {errors.email && <p className="error">Email field is invalid</p>}
                         </div>
                       </div>
                       <div className="form-group">
                         <label htmlFor="Phone" className="col-sm-1 col-xs-12 control-label">Phone</label>
                               <div className="row" style={{paddingLeft: "15px", paddingRight: "15px"}}>
                                 <div className="col">
-                                  <input type="text" className="form-control" id="phone1" name="phone1" size="3" maxLength="3" />
+                                  <input type="text" onChange={e => setFormVal({ ...formValues, phone1: e.target.value})} className="form-control" ref={register({pattern: /^\d{3}$/})} id="phone1" name="phone1" size="3" maxLength="3" />
+                                  {errors.phone1 && <p className="error">Phone field is invalid</p>}
                                 </div>
                                 <div className="col">
-                                  <input type="text" className="form-control" id="phone2" name="phone2" size="3" maxLength="3" />
+                                  <input type="text" onChange={e => setFormVal({ ...formValues, phone2: e.target.value})} className="form-control" ref={register({pattern: /^\d{3}$/})} id="phone2" name="phone2" size="3" maxLength="3" />
+                                  {errors.phone2 && <p className="error">Phone field is invalid</p>}
                                 </div>
                                 <div className="col">
-                                  <input type="text" className="form-control" id="phone3" name="phone3" size="4" maxLength="4" />
+                                  <input type="text" onChange={e => setFormVal({ ...formValues, phone3: e.target.value})} className="form-control" ref={register({pattern: /^\d{4}$/})} id="phone3" name="phone3" size="4" maxLength="4" />
+                                  {errors.phone3 && <p className="error">Phone field is invalid</p>}
                                 </div>
-                                <div className="phone_valid_error">Phone field is invalid.</div>
                               </div>
                       </div>
                       <div className="form-group">
                         <label htmlFor="Message" className="col-sm-1 control-label"><span className="required">*</span>Message</label>
                         <div className="col-sm-12">
-                            <textarea className="form-control" id="message" name="message" rows="3" maxLength="1000"></textarea>
-                            <div className="message_error">Message is a required field.</div>
-                            <div className="message_valid_error">Message field is invalid.</div>
+                            <textarea onChange={e => setFormVal({ ...formValues, message: e.target.value})} className="form-control" ref={register({required: true, minLength: 2, pattern: /^['-\w\.$!\d,?/@();]+['-\w\s\.$!\d,?/@();]+$/})} id="message" name="message" rows="3" maxLength="1000"></textarea>
+                            {errors.message && <p className="error">Message field is invalid</p>}
                         </div>
                       </div>
                   
