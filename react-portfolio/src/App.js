2b4002a896ca01bb9a8b516acc7e7bfac516c47f
Nick Romano <njr7romano@yahoo.com>
2020-07-15 12:35:18 -0400

    Improved routing

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -16,7 +16,6 @@ function App() {
         <Route exact path="/about" component={About} />
         <Route exact path="/skills" component={Skills} />
         <Route exact path="/contact" component={Contact} />
-        <Route path="/" component={Portfolio} />
         <Route path="*" component={Portfolio} />
       </Switch>
       <Footer />
