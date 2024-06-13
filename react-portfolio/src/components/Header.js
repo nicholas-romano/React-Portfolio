c813559fc0cb6494979605969e797aedaa8829a5
Nicholas Romano <nicholasromano@Nicholass-MBP.attlocal.net>
2024-06-13 14:59:16 -0400

    Updated portfolio

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -6,11 +6,11 @@ const Header = () => {
     <header>   
         <div id="banner">
           <h1>Nicholas Romano</h1>
-          <h3>Full Stack Web Developer</h3>
+          <h3>Web Designer</h3>
           <div className="resume-download">
             <label><a href="mailto:njromano77@gmail.com">njromano77@gmail.com</a></label>&nbsp; &bull; &nbsp;
             <label><a href="https://github.com/nicholas-romano">GitHub</a></label>&nbsp; &bull; &nbsp;
-            <label><a href="https://www.linkedin.com/in/nicholas-romano77">LinkedIn</a></label>&nbsp; &bull; &nbsp;
+            <label><a href="https://www.linkedin.com/in/nicholas-romano77/">LinkedIn</a></label>&nbsp; &bull; &nbsp;
             <label><a href={process.env.PUBLIC_URL + '/Documents/Nicholas-Romano-Resume.pdf'}>Resumé</a></label>
           </div>
           <Nav />
