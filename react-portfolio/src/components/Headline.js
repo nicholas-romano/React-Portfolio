f48beae4b71c4c1dd15ee7ea6607025f75627292
Nicholas Romano <nicholasromano@Nicholass-MBP.attlocal.net>
2024-06-17 18:39:53 -0400

    Updated headline

--- react-portfolio/src/components/Headline.js
+++ react-portfolio/src/components/Headline.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 function Headline() {
+
   return (
-    <section id="headline">
+    <section id="headline" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/images/headline_gears_img.jpg'})` }}>
         <div className="headline-text">
             <h1>Web Design and Development</h1>
             <h5>Building outstanding websites that give your business an edge</h5>
@@ -10,5 +11,7 @@ function Headline() {
     </section>
   );
 }
+
+
 
 export default Headline;
