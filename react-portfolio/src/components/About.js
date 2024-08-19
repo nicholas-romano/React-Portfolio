fae9634b2ba050439d36979eca4748b33e9151e0
Nicholas Romano <njr7romano@yahoo.com>
2024-08-19 17:46:11 -0400

    Added a new project Song-Search app

--- react-portfolio/src/components/About.js
+++ react-portfolio/src/components/About.js
@@ -1,35 +1,43 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
   return (
     <main>
-        <section className="content_section">
-          <section className="column">
-
-            <div className="row">
-              <div className="col-lg-6">
-                
-                  <div className="column-content bio">
-                    <h1>About</h1>
-                    <p>Web Designer and Developer educated at the University of North Carolina and James Madison University. 
-                      Skilled in design prototyping software such as Figma, and the Adobe Creative Suite including Photoshop, 
-                      Illustrator, and XD and skilled in programming web technologies such as HTML, CSS, JavaScript, JQuery and 
-                      Bootstrap, and architectures such as ReactJS, ExpressJS, and NodeJS. Strengths include a passion for 
-                      creativity, dedication to problem-solving, and commitment to quality work.
-                    </p>
-                  </div>
-              </div>
-              <div className="col-lg-6">
-                  <div className="column-content">
-                      <img src={process.env.PUBLIC_URL + "/images/Portfolio-About-Photo.JPG"} className="img-fluid" alt="Nick Romano Profile" />
-                  </div>
+      <section className="content_section">
+        <section className="column">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="column-content bio">
+                <h1>About</h1>
+                <p>
+                  Full Stack Web Developer with graphic design experience
+                  educated at the University of North Carolina and James Madison
+                  University. Skilled in responsive web technologies such as
+                  HTML5, CSS3, JavaScript, JQuery and Bootstrap, and
+                  architectures such as ReactJS, ExpressJS, NodeJS and ASP.NET,
+                  and back-end data modeling technologies such as Sequelize and
+                  Mongoose in connection to back-end databases such as MongoDB
+                  and MySQL. Strengths include a passion for creativity,
+                  dedication to problem-solving, and commitment to quality work.
+                  Excited about the next opportunity to leverage his skills and
+                  experience to assist a company or organization succeed.
+                </p>
               </div>
             </div>
-
-          </section>
+            <div className="col-lg-6">
+              <div className="column-content">
+                <img
+                  src={`${process.env.PUBLIC_URL}/images/Portfolio-About-Photo.JPG`}
+                  className="img-fluid"
+                  alt="Nick Romano Profile"
+                />
+              </div>
+            </div>
+          </div>
         </section>
+      </section>
     </main>
   );
-}
+};
 
 export default About;
