afdd1d733955352d368cc8ff73dd644da04b3c81
Nicholas Romano <njr7romano@yahoo.com>
2024-09-09 11:48:38 -0400

    Removed unused imports

--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Headline from "./Headline";
-import MobileAppSlideshowList from "./MobileAppSlideshowList";
-import DesktopDesignSlideshowList from "./DesktopDesignSlideshowList";
 import ProjectList from "./ProjectList";
 
 const Portfolio = () => {
