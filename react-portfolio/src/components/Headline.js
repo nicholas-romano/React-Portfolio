ba32754d82b2270334b82fc0fb1a2d4ded94ed20
Nicholas Romano <njr7romano@yahoo.com>
2025-01-11 13:04:39 -0500

    Added featured project section

--- react-portfolio/src/components/Headline.js
+++ react-portfolio/src/components/Headline.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React from "react";
 
 function Headline() {
-
   return (
-    <section id="headline" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/images/headline_gears_img.jpg'})` }}>
-        <div className="headline-text">
-            <h1>Web Design and Development</h1>
-            <h5>Building outstanding websites that give your business an edge</h5>
+    <section
+      id="headline"
+      style={{
+        backgroundImage: `url(${
+          process.env.PUBLIC_URL +
+          "/images/headline-photos/The-Wild-Oasis-Cover.png"
+        })`,
+      }}
+    >
+      <div class="headline-text">
+        <div class="headline-title">
+          <h1>The Wild Oasis</h1>
+          <h3>Welcome to paradise</h3>
         </div>
+
+        <div class="headline-subtext">
+          <h5>
+            My latest featured project is a luxury cabin rental application with
+            beautiful cabin photos, search filters, interactive calendars, and
+            booking managment forms.
+          </h5>
+        </div>
+
+        <a
+          href="https://the-wild-oasis-swart-mu.vercel.app/"
+          target="_blank"
+          title="The Wild Oasis"
+          rel="noopener noreferrer"
+        >
+          <button type="button" class="btn btn-primary">
+            See project &gt;&gt;
+          </button>
+        </a>
+      </div>
     </section>
   );
 }
-
-
 
 export default Headline;
